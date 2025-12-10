fbacb4b29b905be941dcb656840f6d3a8326fb56
David <dmaharjan224@gmail.com>
2025-12-10 07:17:00 +0545

    commit from cursor

---
+++ first_test.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import sync_playwright
+with sync_playwright() as p:
+        # Launch the browser (set headless=False to see it)
+        browser = p.chromium.launch(headless=False)
+        
+        # Create a new browser page
+        page = browser.new_page()
+        
+        # Navigate to Google
+        page.goto("https://www.google.com")
+        
+        # Wait a bit so you can see the page
+        page.wait_for_timeout(3000)
+        
+        # Close the browser
+        browser.close()
